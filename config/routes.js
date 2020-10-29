1c4cf20f6b1107586048e28e00699c4ce20b90dc
planimir77 <planimir7708@gmail.com>
2020-10-29 16:11:13 +0200

    add guest home page

--- config/routes.js
+++ config/routes.js
@@ -11,69 +11,69 @@ const handleValidationErrors = require('../express-validations/handle-validation
 module.exports = (app) => {
     app.get('/', homeController.get.homePage);
 
-    app.get('/about', (req, res) => {
-        res.render('about', { title: "About page", });
-    });
+    // app.get('/about', (req, res) => {
+    //     res.render('about', { title: "About page", });
+    // });
 
-    // ********************* Cube *********************
-    // Details
-    app.get('/cube/details/:id', cubeController.get.details);
-    // Create
-    app.get('/cube/create', checkAuth(true), cubeController.get.create);
-    app.post('/cube/create',
-        checkAuth(true),
-        validateCube.name,
-        validateCube.description,
-        validateCube.imageUrl,
-        handleValidationErrors,
-        cubeController.post.create
-    );
-    // Edit
-    app.get('/cube/edit/:id', checkAuth(true), cubeController.get.update);
-    app.post('/cube/edit/:id',
-        checkAuth(true),
-        validateCube.name,
-        validateCube.description,
-        validateCube.imageUrl,
-        handleValidationErrors,
-        cubeController.post.update
-    );
-    // Delete
-    app.get('/cube/delete/:id', checkAuth(true), cubeController.get.delete);
-    app.post('/cube/delete/:id', checkAuth(true), cubeController.post.delete);
-    // Attach Accessory
-    app.get('/cube/attach/accessory/:id', checkAuth(true), cubeController.get.attachAccessory);
-    app.post('/cube/attach/accessory/:id', checkAuth(true), cubeController.post.attachAccessory);
+    // // ********************* Cube *********************
+    // // Details
+    // app.get('/cube/details/:id', cubeController.get.details);
+    // // Create
+    // app.get('/cube/create', checkAuth(true), cubeController.get.create);
+    // app.post('/cube/create',
+    //     checkAuth(true),
+    //     validateCube.name,
+    //     validateCube.description,
+    //     validateCube.imageUrl,
+    //     handleValidationErrors,
+    //     cubeController.post.create
+    // );
+    // // Edit
+    // app.get('/cube/edit/:id', checkAuth(true), cubeController.get.update);
+    // app.post('/cube/edit/:id',
+    //     checkAuth(true),
+    //     validateCube.name,
+    //     validateCube.description,
+    //     validateCube.imageUrl,
+    //     handleValidationErrors,
+    //     cubeController.post.update
+    // );
+    // // Delete
+    // app.get('/cube/delete/:id', checkAuth(true), cubeController.get.delete);
+    // app.post('/cube/delete/:id', checkAuth(true), cubeController.post.delete);
+    // // Attach Accessory
+    // app.get('/cube/attach/accessory/:id', checkAuth(true), cubeController.get.attachAccessory);
+    // app.post('/cube/attach/accessory/:id', checkAuth(true), cubeController.post.attachAccessory);
 
-    /********************* Accessory *********************/
-    app.get('/accessory/create', checkAuth(true), accessoryController.get.create);
-    app.post('/accessory/create',
-        checkAuth(true),
-        validateAccessory.name,
-        validateAccessory.description,
-        validateAccessory.imageUrl,
-        accessoryController.post.create
-    );
+    // /********************* Accessory *********************/
+    // app.get('/accessory/create', checkAuth(true), accessoryController.get.create);
+    // app.post('/accessory/create',
+    //     checkAuth(true),
+    //     validateAccessory.name,
+    //     validateAccessory.description,
+    //     validateAccessory.imageUrl,
+    //     accessoryController.post.create
+    // );
 
-    /********************* User *********************/
-    // Register
-    app.get('/user/register', checkAuth(false), userController.get.register);
-    app.post('/user/register',
-        checkAuth(false),
-        validateUser.username,
-        validateUser.password,
-        handleValidationErrors,
-        userController.post.register
-    );
-    // Login
-    app.get('/user/login', checkAuth(false), userController.get.login);
-    app.post('/user/login', checkAuth(false), userController.post.login);
+    // /********************* User *********************/
+    // // Register
+    // app.get('/user/register', checkAuth(false), userController.get.register);
+    // app.post('/user/register',
+    //     checkAuth(false),
+    //     validateUser.username,
+    //     validateUser.password,
+    //     handleValidationErrors,
+    //     userController.post.register
+    // );
+    // // Login
+    // app.get('/user/login', checkAuth(false), userController.get.login);
+    // app.post('/user/login', checkAuth(false), userController.post.login);
 
-    // Logout
-    app.get('/logout', checkAuth(true), userController.get.logout);
+    // // Logout
+    // app.get('/logout', checkAuth(true), userController.get.logout);
 
-    /****************** Not found *********************/
-    app.get('*', (req, res) => {
-        res.render('404', { title: "Not found", });
-    });
+    // /****************** Not found *********************/
+    // app.get('*', (req, res) => {
+    //     res.render('404', { title: "Not found", });
+    // });
 };
--- controllers/home.js
+++ controllers/home.js
@@ -4,8 +4,8 @@ module.exports = {
         async homePage(req, res) {
             const query = req.query;
             try {
-                const cubes = await getCubes(query);
-                res.render('index', { title: "Home page", cubes: cubes, });
+                //const cubes = await getCubes(query);
+                res.render('index', { title: "Home page", });
             } catch (error) {
                 console.error('Error :', error);
                 res.render('index', { title: "Home page", errorMessage: error.message, });
